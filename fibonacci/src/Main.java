5ce9ef4391e055d80cd2e402983bdf4e400b6acd
Gabriel Bonaretti da Silva <45726815890>
2023-10-17 14:15:27 -0300

    terminei o fibonacci

--- fibonacci/src/Main.java
+++ fibonacci/src/Main.java
@@ -1,14 +1,14 @@
-import java.util.Scanner;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String continueGame = "S";
-        Menu menu = new Menu();
-        while (continueGame.equals("S")) {
-            menu.printMenu();
-
-            continueGame = menu.isFinished();
+        long [] sequenciaFibonacci = new long[21];
+        sequenciaFibonacci[0] = 0;
+        sequenciaFibonacci[1] = 1;
+        for (int i = 2; i < sequenciaFibonacci.length; i++) {
+            sequenciaFibonacci[i] = sequenciaFibonacci[i-2]+sequenciaFibonacci[i-1];
         }
+
+        System.out.println(Arrays.toString(sequenciaFibonacci));
     }
 }
