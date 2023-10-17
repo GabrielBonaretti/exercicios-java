f0527f21693f84b8bad9353a77a3c1f060f282c4
Gabriel Bonaretti da Silva <45726815890>
2023-10-17 14:04:28 -0300

    terminei eliminar metade

--- eliminar metade/src/Main.java
+++ eliminar metade/src/Main.java
@@ -1,14 +1,35 @@
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String continueGame = "S";
-        Menu menu = new Menu();
-        while (continueGame.equals("S")) {
-            menu.printMenu();
+        Random gerador = new Random();
+        int mortes;
+        ArrayList<String> listaPessoas = new ArrayList<>(List.of("gabriel", "eduarda", "julia", "laura", "henrique",
+                "giovana R", "allana", "felipe", "lucas", "angelo",
+                "diego c", "giovana m", "giovana f", "esther",
+                "caio", "kadu", "nic", "diego nerd", "leo", "agatha", "clebinho"));
 
-            continueGame = menu.isFinished();
+        ArrayList<String> listaMortos = new ArrayList<>();
+
+        int totalPessoas = listaPessoas.size();
+
+        if (listaPessoas.size() % 2 == 0) {
+            mortes = listaPessoas.size() / 2;
+        }else{
+            mortes = listaPessoas.size() / 2 + 1;
         }
+        for (int i = 0; i < mortes; i++) {
+            int pessoaMortaIndex = gerador.nextInt(listaPessoas.size());
+            String pessoaMorta = listaPessoas.get(pessoaMortaIndex);
+            listaPessoas.remove(pessoaMorta);
+            listaMortos.add(pessoaMorta);
+        }
+
+        System.out.println("Pessoas totais antes das mortes: " + totalPessoas);
+        System.out.println("Mortes: " + mortes);
+        System.out.println("Sobreviventes: "+listaPessoas);
+        System.out.println("Mortos: " + listaMortos);
     }
 }
