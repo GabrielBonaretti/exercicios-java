9a98d24e60e0e4f651bf90eb7c6a0db280e7a069
Gabriel Bonaretti da Silva <45726815890>
2023-10-17 16:12:22 -0300

    comentando a sequencia de fibonacci

--- fibonacci/src/Main.java
+++ fibonacci/src/Main.java
@@ -2,10 +2,16 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
+        // Armazena a sequência de Fibonacci com 21 elementos.
         long [] sequenciaFibonacci = new long[21];
+
+        // Define os dois primeiros elementos da sequência.
         sequenciaFibonacci[0] = 0;
         sequenciaFibonacci[1] = 1;
+
+        // Preenche o restante da sequência de Fibonacci a partir do terceiro elemento.
         for (int i = 2; i < sequenciaFibonacci.length; i++) {
+            // Calcula o próximo número da sequência somando os dois anteriores.
             sequenciaFibonacci[i] = sequenciaFibonacci[i-2]+sequenciaFibonacci[i-1];
         }
 
