ec2856d5e9edccef192adb9b37443a67e74d4d43
Gabriel Bonaretti <gabriel_bonaretti@hotmail.com>
2023-10-20 11:24:22 -0300

    quase terminando a lista de restaurante, preciso so paginar

--- delivery/src/UI/Layout/RestauranteLabel.java
+++ delivery/src/UI/Layout/RestauranteLabel.java
@@ -5,28 +5,29 @@ import java.awt.*;
 
 public class RestauranteLabel extends JLabel {
 
-    public RestauranteLabel() {
-        this.setBounds(125, 150, 500, 50);
+    public RestauranteLabel(String restaurantName, String restaurantPosition, int y) {
+        this.setBounds(125, y, 500, 50);
         this.setBackground(new Color(180,180,180));
         this.setOpaque(true);
 
         JLabel labelImage = new JLabel();
-        labelImage.setBounds(25, 0, 50, 50);
+        labelImage.setBounds(35, 0, 50, 50);
 
         ImageIcon image = new ImageIcon("src/Resources/lunch.png");
         image.setImage(image.getImage().getScaledInstance(40,40, Image.SCALE_DEFAULT));
 
-        JLabel textName = new JLabel("Nome do restaurante");
+        JLabel textName = new JLabel(restaurantName);
         textName.setFont(new Font("Arial", Font.BOLD,15));
-        textName.setBounds(100, 0, 150, 50);
+        textName.setBounds(110, 0, 150, 50);
 
-        JLabel textPosition = new JLabel("5, 2");
+        JLabel textPosition = new JLabel(restaurantPosition);
         textPosition.setFont(new Font("Arial", Font.BOLD,15));
-        textPosition.setBounds(300, 0, 25, 50);
+        textPosition.setBounds(295, 0, 25, 50);
 
         JButton buttonRestaurant = new JButton("Entrar");
         buttonRestaurant.setFont(new Font("Arial", Font.BOLD,15));
-        textPosition.setBounds(350, 10, 50, 30);
+        buttonRestaurant.setBounds(360, 10, 100, 30);
+        buttonRestaurant.setFocusable(false);
 
 
         labelImage.setIcon(image);
